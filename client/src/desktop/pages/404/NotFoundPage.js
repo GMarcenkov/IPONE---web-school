591b492357452d18501c69fea5c24c5e4b0b9c6a
Nikolas9898 <nikola@3web-studio.com>
2020-10-12 14:02:49 +0300

    added 404 page

--- client/src/desktop/pages/404/NotFoundPage.js
+++ client/src/desktop/pages/404/NotFoundPage.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Link } from "react-router-dom";
+import NotFoundCSS from "./NotFoundPage.module.css";
 
-const NotFoundPage = props => {
-    return (
-        <div>
-    Преебан си
+const NotFoundPage = (props) => {
+  return (
+    <>
+      <div className={NotFoundCSS.mainbox}>
+        <div className={NotFoundCSS.err}>4</div>
+        <div className={NotFoundCSS.err0}>0</div>
+        <div className={NotFoundCSS.err2}>4</div>
+        <div className={NotFoundCSS.msg}>
+          Maybe this page moved? Got deleted? Is hiding out in quarantine? Never
+          existed in the first place?
+          <p>
+            Let's go{" "}
+            <Link to="/" className={NotFoundCSS.home}>
+              Home
+            </Link>{" "}
+            and try from there.
+          </p>
         </div>
-    );
-};
-
-NotFoundPage.propTypes = {
-
+      </div>
+    </>
+  );
 };
 
 export default NotFoundPage;
