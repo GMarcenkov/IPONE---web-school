d0b1005f1d00cf7ffbcc6b19678ab87efed276fc
georgi.marcenkov <georgi.marcenkov@3web-studio.com>
2020-11-14 19:24:35 +0200

    4

--- src/desktop/modules/login/Login.js
+++ src/desktop/modules/login/Login.js
@@ -65,16 +65,12 @@ class Login extends React.Component {
       });
   };
   render() {
+
     return (
       <div className="form">
         {" "}
-        <Helmet>
-        <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
-        <meta property="og:type"               content="article" />
-        <meta property="og:title"              content="When Great Minds Don’t Think Alike" />
-        <meta property="og:image:secure_url"        content="How much does culture influence creative thinking?" />
-        <meta property="og:description"        content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
-        <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+            <Helmet>
+                        <meta property="og:description"   content=" te tova e opisanieto " />
             </Helmet>
         <div className="logo">
          IPON WEB SCHOOL
