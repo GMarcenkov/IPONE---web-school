53855e24471f03ef34186533d4203a1a50a0e1ea
georgi.marcenkov <georgi.marcenkov@3web-studio.com>
2020-12-10 19:53:36 +0200

    add ga

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React,{Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import './App.css';
 import Routes from "./desktop/routes";
 import AdminRoutes from "./admin/routes";
+import ReactGa from  'react-ga';
 
 function App() {
+    useEffect(()=>{
+        ReactGa.initialize("G-JE6QZ9D1JS")
+        ReactGa.pageview('/login')
+    },[])
+
   return (
     <Fragment>
       <Routes/>
